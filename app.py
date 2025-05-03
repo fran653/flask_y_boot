a12408decb9d7676fdb8176f8837537c28dfb6f7
Topot <fran653@hotmail.com>
2025-05-04 00:08:00 +0200

    updates varios

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 #creo instancia de flask
 app = Flask(__name__)
-@app.route('') #url: http://localhost:5000/
-def inicio():
+titulo_app = 'Puto BASIC fit'
+@app.route('/') #url: http://localhost:5000/
+@app.route('/index.html') #url http://localhost:5000/index.html añado otra ruta más
+def inicio(): #en modo debug se ven cambios de forma automática
     app.logger.debug('entramos al path de inicio /')
-    return '<>hola negro </p>'
+    return render_template('index.html', titulo = titulo_app) #renderiza el template index.html
 
 if __name__ =='__main__':
     app.run(debug=True)
